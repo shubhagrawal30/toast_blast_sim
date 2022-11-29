08d9c43ab3a448db05d320e6a624d5c9a6b10969
Shubh Agrawal <shubh@sas.upenn.edu>
2022-11-28 23:55:49 -0500

    get a schedule

--- create_schedule.sh
+++ create_schedule.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-# Just for fun, picking B2K deep field
+# changed to 6.5 degree patch
 target_ra=82.0
 target_dec=-45.0
-target_width=15.0
+target_width=6.5
 
 toast_ground_schedule \
---site-lat -77.85 \
---site-lon 166.69 \
+--site-lat -44.7 \
+--site-lon 169.15 \
 --site-alt 36500 \
 `# This is a hack for weather stuff` \
---site-name south_pole \
---telescope BLAST \
+--site-name wanaka_nz \
+--telescope BLAST-O \
 --patch-coord C \
---el-min-deg 40 \
---el-max-deg 65 \
---sun-el-max-deg 90 \
---sun-avoidance-angle-deg 45 \
---moon-avoidance-angle-deg 45 \
---start "2023-01-01 00:00:00" \
---stop "2023-01-14 00:00:00" \
---gap-s 300 \
+--el-min-deg 30 \
+--el-max-deg 80 \
+--start "2027-03-19 00:00:00" \
+--stop "2027-04-10 00:00:00" \
 --ces-max-time-s 1200 \
---fp-radius-deg 5.0 \
+--fp-radius-deg 0.75 \
 --patch SIMTARGET,1.0,${target_ra},${target_dec},${target_width} \
---out blast_schedule.txt
+--out blast_schedule.txt \
+--debug
